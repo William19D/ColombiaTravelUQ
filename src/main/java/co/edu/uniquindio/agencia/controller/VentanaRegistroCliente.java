e464260dba5b15c8a22576244b33f148ac5903f5
camila21dun <127644009+camila21dun@users.noreply.github.com>
2023-11-06 16:40:03 -0500

    ventana registro cliente

--- src/main/java/co/edu/uniquindio/agencia/controller/VentanaRegistroCliente.java
+++ src/main/java/co/edu/uniquindio/agencia/controller/VentanaRegistroCliente.java
@@ -1,4 +1,54 @@
 package co.edu.uniquindio.agencia.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class VentanaRegistroCliente {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private Button btnRegistrarCliente;
+
+    @FXML
+    private TextField txtCedula;
+
+    @FXML
+    private TextField txtCorreo;
+
+    @FXML
+    private TextField txtDireccion;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private TextField txtTelefono;
+
+    @FXML
+    void cancelarEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registrarClienteEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
 }
