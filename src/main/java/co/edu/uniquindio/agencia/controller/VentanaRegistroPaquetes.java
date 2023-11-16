730ef4070a141c317f8bbecb26071295310b8fad
Daniel M <danielestebanmunoz1019@gmail.com>
2023-11-16 01:49:45 -0500

    Se crea parcialmente la ventana de destinos TENGO MUCHO SUEÑO Y NO SE QUE HICE AAAAAAAAAAAAAAAAAAAAAAAA

--- src/main/java/co/edu/uniquindio/agencia/controller/VentanaRegistroPaquetes.java
+++ src/main/java/co/edu/uniquindio/agencia/controller/VentanaRegistroPaquetes.java
@@ -1,18 +1,30 @@
 package co.edu.uniquindio.agencia.controller;
 
+import co.edu.uniquindio.agencia.exceptions.AtributoVacioException;
+import co.edu.uniquindio.agencia.exceptions.DestinoRepetidoException;
+import co.edu.uniquindio.agencia.exceptions.InformacionRepetidaException;
+import co.edu.uniquindio.agencia.exceptions.RutaInvalidaException;
+import co.edu.uniquindio.agencia.model.AgenciaViajes;
+import co.edu.uniquindio.agencia.model.Destino;
+import co.edu.uniquindio.agencia.model.PaquetesTuristicos;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Optional;
 
 public class VentanaRegistroPaquetes {
+
+    @FXML
+    private Button agregarEleccion;
 
     @FXML
     private Button btnActualizar;
@@ -30,16 +42,31 @@ public class VentanaRegistroPaquetes {
     private Button btnVolverMenuAdmins;
 
     @FXML
-    private TableColumn<?, ?> columNombre;
+    private TableColumn<Destino, String> columNombreAgregado;
 
     @FXML
-    private TableColumn<?, ?> columnExperiencia;
+    private TableColumn<Destino, String> columNombreDisp;
 
     @FXML
-    private TableColumn<?, ?> columnIdentificacion;
+    private TableColumn<PaquetesTuristicos, String> columNombrePaquete;
 
     @FXML
-    private TableColumn<?, ?> columnIdiomas;
+    private TableColumn<Destino, String> columnCiudadAgregado;
+
+    @FXML
+    private TableColumn<Destino, String> columnCiudadDisp;
+
+    @FXML
+    private TableColumn<Destino, String> columnClimaAgregado;
+
+    @FXML
+    private TableColumn<Destino, String> columnClimaDisp;
+
+    @FXML
+    private TableColumn<PaquetesTuristicos, String> columnCupoPaquete;
+
+    @FXML
+    private TableColumn<PaquetesTuristicos, String> columnPrecioPaquete;
 
     @FXML
     private DatePicker dateFinal;
@@ -48,7 +75,16 @@ public class VentanaRegistroPaquetes {
     private DatePicker dateInicio;
 
     @FXML
-    private TableView<?> tabPaquetesRegistrados;
+    private Button eliminarEleccion;
+
+    @FXML
+    private TableView<Destino> tableDestinosAgregados;
+
+    @FXML
+    private TableView<Destino> tableDestinosDisponibles;
+
+    @FXML
+    private TableView<PaquetesTuristicos> tablePaquetesRegistrados;
 
     @FXML
     private TextField txtCupoMax;
@@ -68,6 +104,75 @@ public class VentanaRegistroPaquetes {
     @FXML
     private AnchorPane ventanaPaquetes;
 
+    private final AgenciaViajes agenciaViajes = AgenciaViajes.getInstance();
+    ObservableList<Destino> listaDisponibles = FXCollections.observableArrayList(AgenciaViajes.getInstance().getDestinos());
+    private ObservableList<Destino> destinosAgregados = FXCollections.observableArrayList();
+
+
+    @FXML
+    void initialize() throws RutaInvalidaException {
+        tablaDestinos();
+
+    }
+
+    public VentanaRegistroPaquetes() throws AtributoVacioException, DestinoRepetidoException, RutaInvalidaException, InformacionRepetidaException {
+    }
+
+    void tablaDestinos() throws RutaInvalidaException {
+
+        tableDestinosDisponibles.setItems(listaDisponibles);
+
+        columNombreDisp.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getNombre()));
+        columnCiudadDisp.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getCiudad()));
+        columnClimaDisp.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getClima().toString()));
+
+    }
+
+    @FXML
+    void agregarEleccion(ActionEvent event) {
+        // Obtener el destino seleccionado en la tabla de destinos disponibles
+        Destino destinoSeleccionado = tableDestinosDisponibles.getSelectionModel().getSelectedItem();
+
+        if (destinoSeleccionado != null) {
+            // Agregar el destino a la lista de destinos agregados
+            destinosAgregados.add(destinoSeleccionado);
+
+            // Eliminar el destino de la tabla de destinos disponibles
+            tableDestinosDisponibles.getItems().remove(destinoSeleccionado);
+
+            // Actualizar la tabla de destinos agregados
+            tableDestinosAgregados.setItems(destinosAgregados);
+            columNombreAgregado.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getNombre()));
+            columnCiudadAgregado.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getCiudad()));
+            columnClimaAgregado.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getClima().toString()));
+
+        }
+    }
+
+    @FXML
+    void eliminarEleccion(ActionEvent event) {
+        // Obtén el destino seleccionado en la tabla de destinos agregados
+        Destino destinoSeleccionado = tableDestinosAgregados.getSelectionModel().getSelectedItem();
+
+        if (destinoSeleccionado != null) {
+            // Muestra un diálogo de confirmación
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmar eliminación");
+            alert.setHeaderText(null);
+            alert.setContentText("¿Está seguro de que desea eliminar este destino?");
+
+            Optional<ButtonType> result = alert.showAndWait();
+
+            if (result.isPresent() && result.get() == ButtonType.OK) {
+                // Remueve el destino de la tabla de destinos agregados
+                tableDestinosAgregados.getItems().remove(destinoSeleccionado);
+
+                // Agrega el destino de nuevo a la lista de destinos disponibles
+                listaDisponibles.add(destinoSeleccionado);
+            }
+        }
+    }
+
     @FXML
     void actualizarEvent(ActionEvent event) {
 
@@ -80,13 +185,85 @@ public class VentanaRegistroPaquetes {
 
     @FXML
     void registrarPaqueteEvent(ActionEvent event) {
+        try {
+            // Obtener los datos necesarios del formulario
+            String nombre = txtNombre.getText();
+            String descripcion = txtDescripcion.getText();
+            String serviciosAdicionales = txtServicios.getText();
+            double precio = Double.parseDouble(txtPrecio.getText());
+            int cupoMax = Integer.parseInt(txtCupoMax.getText());
+            LocalDate fechaDisponibleInicio = dateInicio.getValue();
+            LocalDate fechaDisponibleFin = dateFinal.getValue();
 
+            // Validar y obtener la lista de destinos seleccionados
+            ObservableList<Destino> destinosSeleccionados = tableDestinosAgregados.getItems();
+            if (destinosSeleccionados.isEmpty()) {
+                mostrarError("Debe seleccionar al menos un destino para el paquete.");
+                return;
+            }
+
+            // Realizar otras validaciones según tus requisitos
+
+            // Llamar al método de la instancia de AgenciaViajes para registrar el paquete
+            PaquetesTuristicos paqueteRegistrado = agenciaViajes.registrarPaquetes(
+                    nombre,
+                    new ArrayList<>(destinosSeleccionados),
+                    descripcion,
+                    serviciosAdicionales,
+                    precio,
+                    cupoMax,
+                    fechaDisponibleInicio,
+                    fechaDisponibleFin,
+                    null // Aquí debes pasar el guía correspondiente si lo tienes en tu interfaz de usuario
+            );
+
+            // Limpiar los campos después de registrar el paquete
+            limpiarCampos();
+
+            // Notificar al usuario
+            mostrarInformacion("Paquete registrado exitosamente: " + paqueteRegistrado.getNombre());
+        } catch (AtributoVacioException | InformacionRepetidaException | RutaInvalidaException e) {
+            mostrarError(e.getMessage());
+        } catch (NumberFormatException e) {
+            mostrarError("Por favor, ingrese un valor numérico válido para el precio y el cupo máximo.");
+        }
     }
+
+    // Método para limpiar los campos del formulario después de registrar un paquete
+    private void limpiarCampos() {
+        txtNombre.clear();
+        txtDescripcion.clear();
+        txtServicios.clear();
+        txtPrecio.clear();
+        txtCupoMax.clear();
+        dateInicio.setValue(null);
+        dateFinal.setValue(null);
+        destinosAgregados.clear();
+        tableDestinosAgregados.setItems(destinosAgregados);
+    }
+
 
     @FXML
     void volverMenuAdmins(ActionEvent event) throws IOException {
         new ViewController(ventanaPaquetes, "/ventanas/ventanaMenuAdmins.fxml");
 
+    }
+
+    private void mostrarError(String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+
+    // Método para mostrar un mensaje de información al usuario
+    private void mostrarInformacion(String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Información");
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
     }
 
 }
